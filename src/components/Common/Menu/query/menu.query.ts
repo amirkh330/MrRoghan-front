24363b4bf68fa45c411f0f63021b320a502fee8b
amir hossein khakbaz <a.khakbaz@gmail.com>
2025-12-26 17:10:39 +0330

    fix api msg count

--- src/components/Common/Menu/query/menu.query.ts
+++ src/components/Common/Menu/query/menu.query.ts
@@ -7,10 +7,20 @@ export const useGetMessageCount = ({ isShop }: { isShop: boolean }) => {
 
   return useQuery({
     queryKey: ["message-count", isShop],
+
     queryFn: async () => {
       const res = await api.get<IApiResponse<any>>(`/shops/message-count`);
       return res.data.data;
     },
+
     enabled: isShop,
+
+    // 🔥 مهم‌ترین تنظیمات
+    staleTime: 0, // دیتا همیشه stale
+    gcTime: 0, // (React Query v5) = بدون کش
+    refetchOnMount: "always", // با هر mount
+    refetchOnWindowFocus: true,
+    refetchOnReconnect: true,
+    retry: false,
   });
 };
