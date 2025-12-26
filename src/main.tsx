2843a0af32684cee5f77aa8eccde44da88bfccee
amir hossein khakbaz <a.khakbaz@gmail.com>
2025-12-26 14:05:38 +0330

    fix refresh

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,15 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { customTheme } from "./settings/customTheme";
 import { QueryClient } from "@tanstack/react-query";
 
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then((reg) => console.log("SW registered", reg))
+      .catch((err) => console.log("SW registration failed", err));
+  });
+}
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ChakraProvider theme={customTheme}>
